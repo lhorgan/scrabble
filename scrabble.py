eaf894f3b00da442799d63ea22e6d1ee31dc694b
Luke Horgan <horgan.l@husky.neu.edu>
2018-12-08 10:41:43 -0500

    generate words with fixed characters

--- scrabble.py
+++ scrabble.py
@@ -1,6 +1,5 @@
-def build_dict_from_file(filename):
-    dict = {}
-    print("reading dictionary...")
+def build_dct_from_file(filename):
+    print("reading dctionary...")
     with open(filename) as f:
         words = f.readlines()
     
@@ -9,14 +8,14 @@ def build_dict_from_file(filename):
 
     return suffix_tree
 
-def is_word(s, dict):
-    node = get_node(s, dict)
+def is_word(s, dct):
+    node = get_node(s, dct)
     if node:
         return node[1]
     return False
 
-def get_node(s, dict):
-    st = dict
+def get_node(s, dct):
+    st = dct
     super_tree = None
     for c in s:
         if c in st:
@@ -26,10 +25,10 @@ def get_node(s, dict):
             return None
     return super_tree
 
-def get_all_words(letters, dict):
+def get_all_words(letters, dct):
     stack = []
     for i in range(len(letters)):
-        node = get_node(letters[i], dict)
+        node = get_node(letters[i], dct)
         rest = letters[:i] + letters[i+1:]
         stack.append([letters[i], rest, node])
     
@@ -50,6 +49,42 @@ def get_all_words(letters, dict):
     
     return words
 
+def get_all_words2(letters, template, dct):
+    stack = []
+
+    if template[0] == "*":
+        for i in range(len(letters)):
+            node = get_node(letters[i], dct)
+            rest = letters[:i] + letters[i+1:]
+            stack.append([letters[i], rest, node])
+    else:
+        node = get_node(template[0], dct)
+        rest = letters
+        stack.append([template[0], rest, node])
+    
+    words = set()
+    while len(stack) > 0:
+        seq, rest, node = stack.pop()
+        if node[1]:
+            words.add(seq)
+
+        if len(seq) < len(template):
+            if template[len(seq)] == "*":
+                for i in range(len(rest)):
+                    new_first = rest[i]
+                    new_rest = rest[:i] + rest[i+1:]
+                    new_node = get_node(new_first, node[0])
+                    if new_node:
+                        stack.append([seq + new_first, new_rest, new_node])
+            else:
+                new_first = template[len(seq)]
+                new_rest = rest
+                new_node = get_node(new_first, node[0])
+                if new_node:
+                    stack.append([seq + new_first, new_rest, new_node])
+    
+    return words
+
 def build_suffix_tree(word_list):
     suffix_tree = {}
     for word in word_list:
@@ -65,4 +100,6 @@ def build_suffix_tree(word_list):
             if i == len(word) - 1:
                 super_tree[c][1] = True
     return suffix_tree
+
+board = []
 
